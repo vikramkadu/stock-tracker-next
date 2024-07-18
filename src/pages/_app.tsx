be2b674b249e233e105f00335dc4b9cc1472d6d5
vikramkadu <vikramkkadu@gmail.com>
2024-07-18 14:35:38 +0530

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import store from '@/redux/store';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
